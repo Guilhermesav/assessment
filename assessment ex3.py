cc8d3ebcd37e911581ca992449f125988ed7c00a
Guilhermesav <54535484+Guilhermesav@users.noreply.github.com>
2019-09-25 22:36:43 -0300

    Update assessment ex3.py

--- assessment ex3.py
+++ assessment ex3.py
@@ -3,9 +3,13 @@
 #(não use a função de python math.pow) e retornar o resultado da operação.
 #Depois, crie um programa em Python que obtenha dois números inteiros do usuário e indique o resultado de AB usando a função.
 def potencia(A,B):
-    potencia = A**B
+    if B == 1:
+        return A
+    else:
+        return A*potencia(A,B-1)
     return potencia
 A = int(input()) #Numero base
 B = int(input()) #Potencia
 
 print("A potencia de",A,"elevado a",B,"é:",potencia(A,B))
+
